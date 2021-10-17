62c257ea604bc9d682a91331fabde274583605b6
Torbjørn Bakke <tbakke93@gmail.com>
2021-10-18 00:41:28 +0200

    prep

--- deploy.sh
+++ deploy.sh
@@ -10,5 +10,5 @@ cd dist
 git init
 git add -A
 git commit -m 'deploy'
-git push -f git@github.com:torbjbak/WebVisualizer.git master:gh-pages
+git push -f git@github.com:torbjbak/WebVisualizer.git main:gh-pages
 cd -
